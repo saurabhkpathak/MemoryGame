7cabcb60f311fd6932db16809897f1a7a5ba0223
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-08 18:55:56 +0530

    initial setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name urbanClapMemoryGameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the urbanClapMemoryGameApp
+ */
+angular.module('urbanClapMemoryGameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
