a0fba7f08f81f070ef2e76398a9031dd5b8aa7a9
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-09 17:18:17 +0530

    adding functionality for existing games

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,8 +9,18 @@
  */
 angular.module('urbanClapMemoryGameApp')
     .controller('MainCtrl', function($scope, $location, $localStorage) {
+
+        function checkForExistingGames() {
+            $scope.gameExists = ($localStorage.gameStatus) ? true : false;
+        }
+
         $scope.createGridAndRedirect = function() {
-            if (($scope.gridSize.row * $scope.gridSize.column) % 2 !== 0) {
+            var numberRegex = /^[0-9]*$/;
+            if (!numberRegex.test($scope.gridSize.row) || !numberRegex.test($scope.gridSize.column)) {
+                $scope.error = 'The value for row and column can only be an integer.';
+                return;
+            } else if (($scope.gridSize.row * $scope.gridSize.column) % 2 !== 0) {
+                $scope.error = 'The product of row and column should be an even integer.';
                 return;
             }
             delete $localStorage.click1;
@@ -18,4 +28,11 @@ angular.module('urbanClapMemoryGameApp')
             $localStorage.gridSize = $scope.gridSize;
             $location.path('/game');
         };
+
+        $scope.redirectToGame = function() {
+            $location.path('/game');
+        };
+
+        // calling initial functions
+        checkForExistingGames();
     });
