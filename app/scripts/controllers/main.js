58f839b65eebf074f4323543de83ad630aa0c623
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-09 17:34:17 +0530

    handling empty inputs issue

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,11 +16,14 @@ angular.module('urbanClapMemoryGameApp')
 
         $scope.createGridAndRedirect = function() {
             var numberRegex = /^[0-9]*$/;
-            if (!numberRegex.test($scope.gridSize.row) || !numberRegex.test($scope.gridSize.column)) {
+            if (!$scope.gridSize || !numberRegex.test($scope.gridSize.row) || !numberRegex.test($scope.gridSize.column)) {
                 $scope.error = 'The value for row and column can only be an integer.';
                 return;
             } else if (($scope.gridSize.row * $scope.gridSize.column) % 2 !== 0) {
                 $scope.error = 'The product of row and column should be an even integer.';
+                return;
+            } else if ($scope.gridSize.row > 9 || $scope.gridSize.column > 9) {
+                $scope.error = 'Max limit for a row or column is 10, greater than this a row will take space for two rows.';
                 return;
             }
             delete $localStorage.click1;
